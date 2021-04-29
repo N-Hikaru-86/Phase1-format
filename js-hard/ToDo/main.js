556ceb1e39c85c955c2c29b331b46701ef24e4d5
N-Hikaru-86 <n.hikaru4616@gmail.com>
2021-04-29 18:30:26 +0900

    空になってたらアラート

--- js-hard/ToDo/main.js
+++ js-hard/ToDo/main.js
@@ -2,8 +2,14 @@ const addArea = document.getElementById("add-area");
 const add_btn = document.getElementsByClassName("add-btn");
 const addBtn = add_btn[0];
 const todo = document.getElementById("todo");
+const todo_app = document.getElementById("todo-app");
+console.log(addArea.length)
 
 addBtn.addEventListener("click", event => {
+    if (addArea.length === undefined) {
+        alert("空です！");
+        return 0;
+    }
     let list = document.createElement('li');
     todo.appendChild(list)
     let text = document.createElement('p');
@@ -12,7 +18,14 @@ addBtn.addEventListener("click", event => {
     delBtn.textContent = "削除する";
     list.appendChild(text);
     text.appendChild(delBtn);
+    // let reborn = text;
     delBtn.addEventListener("click", event => {
         list.parentNode.removeChild(list);
+        // let rebBtn = document.createElement('button');
+        // rebBtn.textContent = "復活する";
+        // todo_app.appendChild(rebBtn);
+        // rebBtn.addEventListener("click", event => {
+        //     list = reborn;
     })
 })
+// })
